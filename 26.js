155f649cd0a1d835cf0d04c63d2e3fec74987045
“sssssuper1” <156980742@qq.com>
2020-08-03 20:14:39 +0800

    26

---
+++ 26.js
@@ -0,0 +1,26 @@
+const removeDuplicates = function(nums) {
+  if (nums.length < 2) return nums.length
+  let i = 0;
+  let j = 0;
+  while (i < nums.length) {
+    if (nums[i] === nums[j]) {
+      i++
+    } else {
+      if (i - j > 1) {
+        j++
+        let tmp = nums[j]
+        nums[j] = nums[i]
+        nums[i] = tmp
+      } else {
+        j++
+      }
+      i++
+    }
+  }
+
+  console.log(nums.slice(0, j + 1))
+
+  return j + 1
+}
+
+console.log(removeDuplicates([0,0,1,1,2,2,3,3,4,4,5,5]))
